9997736522546cf0784490564ddbef5d06e7f0a8
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-05-12 22:50:31 -0500

    Add files via upload

---
+++ ArchivosSecuenciales/ArchivosSecuenciales/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ArchivosSecuenciales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.archivosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.altasDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultaDeDatosPorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generalesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nombresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.especialidadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.semestreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.promedioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificacionDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bajasDeDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.matriculaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.archivosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(888, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(14, 24);
+            // 
+            // archivosToolStripMenuItem
+            // 
+            this.archivosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.altasDeDatosToolStripMenuItem,
+            this.consultaDeDatosPorToolStripMenuItem,
+            this.modificacionDeDatosToolStripMenuItem,
+            this.bajasDeDatosToolStripMenuItem});
+            this.archivosToolStripMenuItem.Name = "archivosToolStripMenuItem";
+            this.archivosToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.archivosToolStripMenuItem.Text = "Archivos";
+            // 
+            // altasDeDatosToolStripMenuItem
+            // 
+            this.altasDeDatosToolStripMenuItem.Name = "altasDeDatosToolStripMenuItem";
+            this.altasDeDatosToolStripMenuItem.Size = new System.Drawing.Size(247, 26);
+            this.altasDeDatosToolStripMenuItem.Text = "Altas de datos";
+            // 
+            // consultaDeDatosPorToolStripMenuItem
+            // 
+            this.consultaDeDatosPorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generalesToolStripMenuItem,
+            this.nombresToolStripMenuItem,
+            this.especialidadToolStripMenuItem,
+            this.semestreToolStripMenuItem,
+            this.promedioToolStripMenuItem,
+            this.matriculaToolStripMenuItem});
+            this.consultaDeDatosPorToolStripMenuItem.Name = "consultaDeDatosPorToolStripMenuItem";
+            this.consultaDeDatosPorToolStripMenuItem.Size = new System.Drawing.Size(247, 26);
+            this.consultaDeDatosPorToolStripMenuItem.Text = "Consulta de datos por...";
+            // 
+            // generalesToolStripMenuItem
+            // 
+            this.generalesToolStripMenuItem.Name = "generalesToolStripMenuItem";
+            this.generalesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.generalesToolStripMenuItem.Text = "Generales";
+            // 
+            // nombresToolStripMenuItem
+            // 
+            this.nombresToolStripMenuItem.Name = "nombresToolStripMenuItem";
+            this.nombresToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.nombresToolStripMenuItem.Text = "Nombres";
+            // 
+            // especialidadToolStripMenuItem
+            // 
+            this.especialidadToolStripMenuItem.Name = "especialidadToolStripMenuItem";
+            this.especialidadToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.especialidadToolStripMenuItem.Text = "Especialidad";
+            // 
+            // semestreToolStripMenuItem
+            // 
+            this.semestreToolStripMenuItem.Name = "semestreToolStripMenuItem";
+            this.semestreToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.semestreToolStripMenuItem.Text = "Semestre";
+            // 
+            // promedioToolStripMenuItem
+            // 
+            this.promedioToolStripMenuItem.Name = "promedioToolStripMenuItem";
+            this.promedioToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.promedioToolStripMenuItem.Text = "Promedio";
+            // 
+            // modificacionDeDatosToolStripMenuItem
+            // 
+            this.modificacionDeDatosToolStripMenuItem.Name = "modificacionDeDatosToolStripMenuItem";
+            this.modificacionDeDatosToolStripMenuItem.Size = new System.Drawing.Size(247, 26);
+            this.modificacionDeDatosToolStripMenuItem.Text = "Modificacion de datos";
+            // 
+            // bajasDeDatosToolStripMenuItem
+            // 
+            this.bajasDeDatosToolStripMenuItem.Name = "bajasDeDatosToolStripMenuItem";
+            this.bajasDeDatosToolStripMenuItem.Size = new System.Drawing.Size(247, 26);
+            this.bajasDeDatosToolStripMenuItem.Text = "Bajas de datos";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(56, 59);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(705, 275);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Matricula";
+            this.columnHeader1.Width = 99;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nombre";
+            this.columnHeader2.Width = 123;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "A. Paterno";
+            this.columnHeader3.Width = 109;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "A. materno";
+            this.columnHeader4.Width = 105;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Especialidad";
+            this.columnHeader5.Width = 109;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Semestre";
+            this.columnHeader6.Width = 73;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Promedio";
+            this.columnHeader7.Width = 75;
+            // 
+            // matriculaToolStripMenuItem
+            // 
+            this.matriculaToolStripMenuItem.Name = "matriculaToolStripMenuItem";
+            this.matriculaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.matriculaToolStripMenuItem.Text = "Matricula";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 478);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem archivosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem altasDeDatosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultaDeDatosPorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generalesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nombresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem especialidadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem semestreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem promedioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matriculaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificacionDeDatosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bajasDeDatosToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
+
---
+++ ArchivosSecuenciales/ArchivosSecuenciales/Secuenciales.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ArchivosSecuenciales
+{
+    class Secuenciales
+    {
+        public string Matricula;
+        public string Nombre;
+        public string Paterno;
+        public string Materno;
+        public string Especialidad;
+        public int Semestre;
+        public int Promedio;
+
+        public string matricula 
+        {
+            set { value = Matricula; }
+            get { return (Matricula); }
+        }
+        public string nombre
+        {
+            set { value = Nombre; }
+            get { return (Nombre); }
+        }
+        public string paterno
+        {
+            set { value = Paterno; }
+            get { return (Paterno); }
+        }
+        public string materno
+        {
+            set { value = Materno; }
+            get { return (Materno); }
+        }
+        public string especialidad
+        {
+            set { value = Especialidad; }
+            get { return (Especialidad); }
+        }
+        public int semestere
+        {
+            set { value = Semestre; }
+            get { return (Semestre); }
+        }
+        public int promedio
+        {
+            set { value = Promedio; }
+            get { return (Promedio); }
+        }
+
+    }
+}
